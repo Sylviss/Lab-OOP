f10edcc9b6c69ab5edbab9b9b769195663b34e27
Sylviss <76834738+Sylviss@users.noreply.github.com>
2024-04-24 10:17:11 +0700

    Update DigitalVideoDisc.java

--- src/DigitalVideoDisc.java
+++ src/DigitalVideoDisc.java
@@ -4,6 +4,8 @@ public class DigitalVideoDisc {
     private String director;
     private int length;
     private float cost;
+    private int id;
+    private static int nbDigitalVideoDiscs = 0;
 
     public void setTitle(String title){
         this.title = title;
@@ -26,18 +28,22 @@ public class DigitalVideoDisc {
 
     public DigitalVideoDisc(String title){
         this.title = title;
+        id = ++nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String title, String category, float cost){
         this(title);
         this.cagetory = category;
         this.cost = cost;
+        id = ++nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String title, String category, String director, float cost){
         this(title, category, cost);
         this.director = director;
+        id = ++nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String title, String category, String director, int length, float cost){
         this(title, category, director, cost);
         this.length = length;
+        id = ++nbDigitalVideoDiscs;
     }
 }
