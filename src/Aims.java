3cf81db4ec453f165d33755acb1388dc76274ea9
Sylviss <76834738+Sylviss@users.noreply.github.com>
2024-04-24 09:34:53 +0700

    Initial

---
+++ src/Aims.java
@@ -0,0 +1,17 @@
+public class Aims {
+    public static void main(String[] args){
+        Cart an0rder = new Cart();
+        
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        an0rder.addDigitalVideoDisc(dvd1);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        an0rder.addDigitalVideoDisc(dvd2);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", 18.99f);
+        an0rder.addDigitalVideoDisc(dvd3);
+        System.out.println("This order has: " + an0rder.getQtyOrdered() + " DVD");
+        System.out.println("Total cost: " + an0rder.totalCost());
+        an0rder.removeDigitalVideoDisc(dvd2);
+        System.out.println("This order has: " + an0rder.getQtyOrdered() + " DVD");
+        System.out.println("Total cost: " + an0rder.totalCost());    
+    }
+}
